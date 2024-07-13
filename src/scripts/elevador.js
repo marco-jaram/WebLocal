7e79c5b91f627bd4f43b5b3aa92c59c53ca5950e
Marco Jaramillo <marco.jaramillocast@gmail.com>
2024-07-13 13:46:59 -0700

    fix prueba btn arriba

--- src/scripts/elevador.js
+++ src/scripts/elevador.js
@@ -1,22 +1,25 @@
-document.getElementById("botonArriba").addEventListener("click", irArriba);
+document.addEventListener("DOMContentLoaded", function() {
+    // Asignar botonArriba después de que el DOM esté cargado
+    let botonArriba = document.getElementById('botonArriba');
 
-function irArriba() {
+    // Agregar event listener después de haber asignado botonArriba
+    botonArriba.addEventListener("click", irArriba);
 
-    let up = document.documentElement.scrollTop;
-    if (up > 0) {
-        window.scrollTo(0, 0);
-    }
-}
-
-botonArriba = document.getElementById('botonArriba');
-window.onscroll = function() {
-
-    let scroll = document.documentElement.scrollTop;
-    if (scroll > 200) {
-        botonArriba.style.transform = "scale(1)";
-
-    } else if (scroll < 200) {
-        botonArriba.style.transform = "scale(0)";
+    // Función para ir arriba
+    function irArriba() {
+        let up = document.documentElement.scrollTop;
+        if (up > 0) {
+            window.scrollTo(0, 0);
+        }
     }
 
-};
+    // Evento de scroll para mostrar/ocultar el botón
+    window.onscroll = function() {
+        let scroll = document.documentElement.scrollTop;
+        if (scroll > 200) {
+            botonArriba.style.transform = "scale(1)";
+        } else {
+            botonArriba.style.transform = "scale(0)";
+        }
+    };
+});
